133091bfa994318e5fd93ee80df1672a738d1855
baidala <stavr@bk.ru>
2016-01-15 17:27:03 +0200

    Create CursorFase.java

---
+++ CursorFase.java
@@ -0,0 +1,14 @@
+package dbms;
+
+public interface CursorFase {
+
+	    public HashMap<String, String> getItem(int pos);
+	    public void createItem(String[] values);
+	    public void select();
+	    public void update(int pos, String[] values);
+	    public void delete(int pos);
+	    public void insert(int pos, String[] values);
+	    public boolean isLast();
+	    
+
+}
