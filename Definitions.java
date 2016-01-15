0bf5aa2b50a1d2a290cc1f63e6dd1d6885b39863
baidala <stavr@bk.ru>
2016-01-15 23:03:09 +0200

    Create Definitions.java

---
+++ Definitions.java
@@ -0,0 +1,11 @@
+package dbms;
+
+public class Definitions {
+	final public static int SELECT_CASE = 1;
+	final public static int INSERT_CASE = 1;
+	final public static int DELETE_CASE = 1;
+	final public static int UPDATE_CASE = 1;
+	final public static int EXIT_CASE = 0;
+	
+
+}
